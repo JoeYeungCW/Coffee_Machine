8f692972934d4aa64b0bf227b910adda2a499b6f
jukka11331 <109426792+jukka11331@users.noreply.github.com>
2022-07-18 03:17:27 +0800

    Add files via upload

---
+++ Part4.java
@@ -0,0 +1,97 @@
+package machine;
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int water = 400;
+        int milk = 540;
+        int bean = 120;
+        int cup  = 9;
+        int money = 550;
+        menu(water,milk,bean,cup,money);
+        action(scanner,water,milk,bean,cup,money);
+
+
+    }
+
+    public static void menu(int water,int milk,int bean,int cup,int money){
+        System.out.println("The coffee machine has:");
+        System.out.printf("%d ml of water%n",water);
+        System.out.printf("%d ml of milk%n",milk);
+        System.out.printf("%d g of coffee beans%n",bean);
+        System.out.printf("%d disposable cups%n",cup);
+        System.out.printf("$%d of money%n",money);
+
+    }
+
+    public static void buy(Scanner scanner,int water,int milk,int bean,int cup,int money){
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        switch (scanner.nextInt()){
+            case 1:
+                water-=250;
+                bean-=16;
+                cup-=1;
+                money+=4;
+                break;
+            case 2:
+                water-=350;
+                milk-=75;
+                bean-=20;
+                cup-=1;
+                money+=7;
+                break;
+            case 3:
+                water-=200;
+                milk-=100;
+                bean-=12;
+                cup-=1;
+                money+=6;
+                break;
+            default:
+                break;
+        }
+        menu(water,milk,bean,cup,money);
+    }
+
+    public static void fill(Scanner scanner,int water,int milk,int bean,int cup,int money){
+        System.out.println("Write how many ml of water you want to add:");
+        water+=scanner.nextInt();
+        System.out.println("Write how many ml of milk you want to add:");
+        milk+=scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        bean+=scanner.nextInt();
+        System.out.println("Write how many disposable cups of coffee you want to add:");
+        cup+=scanner.nextInt();
+        menu(water,milk,bean,cup,money);
+    }
+
+    public static void take(int water,int milk,int bean,int cup,int money){
+        System.out.printf("I gave you $%d%n",money);
+        money=0;
+        menu(water,milk,bean,cup,money);
+    }
+
+    public static void action(Scanner scanner,int water,int milk,int bean,int cup,int money){
+        System.out.println("Write action (buy, fill, take):");
+        String action = scanner.next();
+        switch(action){
+            case "buy":
+                buy(scanner,water,milk,bean,cup,money);
+                break;
+            case "fill":
+                fill(scanner,water,milk,bean,cup,money);
+                break;
+            case "take":
+                take(water,milk,bean,cup,money);
+                break;
+            default:
+                break;
+
+        }
+    }
+
+
+
+}
+
